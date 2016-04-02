511d601327303d359567610c4b77cbb8ca847cbb
Jun <felipe.jun.ohira@gmail.com>
2016-04-01 21:45:42 -0300

    Isolating low level logic on Base World

--- src/js/world/wire/wire_world.js
+++ src/js/world/wire/wire_world.js
@@ -1,6 +1,4 @@
-import Position from '../../value_objects/position';
-
-import CellMap from '../../models/cell_map';
+import BaseWorld from '../base_world';
 
 // World Cell States
 let EMPTY = 'empty';
@@ -8,79 +6,44 @@ let CONDUCTOR = 'conductor';
 let ELETRON = 'eletron';
 let ELETRON_TAIL = 'eletron-tail';
 
-export default class ConwayWorld {
+export default class ConwayWorld extends BaseWorld {
 
   constructor(rows, columns) {
-    this.rows = rows;
-    this.columns = columns;
-
-    this.states = [EMPTY, CONDUCTOR, ELETRON, ELETRON_TAIL];
-
-    this._buildCellMap();
+    super(rows, columns);
   }
 
-  _buildCellMap() {
-    this.cellMap = new CellMap(this.rows, this.columns, EMPTY);
+  getWorldStates() {
+    return [EMPTY, CONDUCTOR, ELETRON, ELETRON_TAIL];
   }
 
-  update() {
-    this._scheduleAllCells();
-    this._updateAllCells();
+  getDefaultCellState() {
+    return EMPTY;
   }
 
-  _scheduleAllCells() {
-    this.cellMap.forEachCell(cell => {
-      this._scheduleCellState(cell);
-    });
+  isCellNotUpdatable(cell) {
+    return cell.hasState(EMPTY);
   }
 
-  _scheduleCellState(cell) {
-
-		if (cell.hasState(ELETRON)) {
+  scheduleCellState(cell) {
+    if (cell.hasState(ELETRON)) {
 			cell.setNextState(ELETRON_TAIL);
 		} else if (cell.hasState(ELETRON_TAIL)) {
 			cell.setNextState(CONDUCTOR);
-		} else if (cell.hasState(CONDUCTOR)) {
-	    let numberOfNeighboursEletrons = this._getNumberOfNeighboursEletronsOfCell(cell);
-			if (numberOfNeighboursEletrons === 1 || numberOfNeighboursEletrons === 2) {
-				cell.setNextState(ELETRON);
-			}
+		} else if (cell.hasState(CONDUCTOR)
+                && this._doesCellHasEnoughEletronsNeighbours(cell)) {
+			cell.setNextState(ELETRON);
 		}
   }
 
+  _doesCellHasEnoughEletronsNeighbours(cell) {
+    let neighboursEletrons = this._getNumberOfNeighboursEletronsOfCell(cell);
+    return neighboursEletrons === 1 || neighboursEletrons === 2;
+  }
+
   _getNumberOfNeighboursEletronsOfCell(cell) {
-    let neighboursPositions = this._getValidNeighboursPositions(cell.position);
-    let liveNeighboursCells = neighboursPositions.filter(position => {
-      return this.cellMap.getCellOnPosition(position).hasState(ELETRON);
-    });
-
-    return liveNeighboursCells.length;
-  }
-
-  _getValidNeighboursPositions(position) {
-    return Position.getMooreNeighborhoodForPosition(position)
-      .filter(neighbourPosition => {
-        return neighbourPosition.x >= 0 &&
-                neighbourPosition.x < this.cellMap.columns &&
-                neighbourPosition.y >= 0 &&
-                neighbourPosition.y < this.cellMap.rows;
-      });
-  }
-
-  _updateAllCells() {
-    this.cellMap.forEachCell(cell => { cell.updateState(); });
-  }
-
-  forEachCell(iterator) {
-    this.cellMap.forEachCell(iterator);
-  }
-
-  getCellOnPosition(position) {
-    return this.cellMap.getCellOnPosition(position);
-  }
-
-  reset() {
-    this._buildCellMap();
+    return this.getNeighboursCellsOfCell(cell)
+            .filter(cell => cell.hasState(ELETRON))
+            .length;
   }
 
 }
